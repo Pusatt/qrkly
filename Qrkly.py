db06ab20fe252d60b7fe7b5b17746beee1d32349
Pusatt <73459501+Pusatt@users.noreply.github.com>
2025-03-22 18:34:08 +0300

    Update Qrkly.py

--- Qrkly.py
+++ Qrkly.py
@@ -2,13 +2,13 @@ import requests
 import time
 
 # İncelenecek ürün numaraları ve aranacak anahtar kelimeler. Test icin start_product = 569
-start_product = 1 
+start_product = 1
 end_product = 17200
 keywords = ['titan', 'titan (holo)', 'ibuypower', 'katowice 2014', '(holo) | katowice 2015', '(holo) | cologne 2014', '(holo) | dreamhack 2014', 'harp of war (holo)', 'king on the field', 'winged defuser', 'howling dawn', 'crown (foil)']
 
 for product_id in range(start_product, end_product + 1):
     url = f"https://gw.bynogame.com/steam-listings/v2/listings?limit=1000&page=1&filters=Product:{product_id}&Sort=Price:1;Score:-1"
-    print(f"\nKontrol edilen urun numarasi: {product_id} ")
+    ### print(f"\nKontrol edilen urun numarasi: {product_id} ")
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -30,7 +30,7 @@ for product_id in range(start_product, end_product + 1):
                     # Ürün adını "product" altındaki "marketHashName" alanından çekiyoruz.
                     product_name = listing.get('product', {}).get('marketHashName', 'İsim Yok')
                     price = listing.get('price', 'Fiyat Yok')
-                    print(f"\n**********************************************\nESLESME BULUNDU: {product_name}\nFiyat: {price}\nStickerlar: {sticker_names}\n\nAnahtar Kelime(ler): {', '.join(matching_keywords)}\n**********************************************\n")
+                    print(f"\nKontrol edilen urun numarasi: {product_id}\n**********************************************\nESLESME BULUNDU: {product_name}\nFiyat: {price}\nStickerlar: {sticker_names}\n\nAnahtar Kelime(ler): {', '.join(matching_keywords)}\n**********************************************\n")
         else:
             print(f"Urun Numarasi {product_id} icin istek hatasi: {response.status_code}")
     except Exception as e:
